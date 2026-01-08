389a58ea69c3627ec49dd3722c5951546413c9e9
Raka Anugrah Satya <147780612+kaadutz@users.noreply.github.com>
2026-01-08 08:00:58 +0700

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom' // Import ini
 import App from './App.tsx'
 import './index.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <BrowserRouter> {/* Bungkus App dengan ini */}
+      <App />
+    </BrowserRouter>
   </StrictMode>,
 )
